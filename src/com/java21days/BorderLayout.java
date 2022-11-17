531069a5277770491397a8bdf74488b1ca48adcd
Alejandro Salas <asalas.wd@outlook.com>
2022-11-16 20:10:54 -0500

    Add constructor

--- src/com/java21days/BorderLayout.java
+++ src/com/java21days/BorderLayout.java
@@ -1,7 +1,19 @@
 package com.java21days;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class BorderLayout extends JFrame {
+    Dimension dim = new Dimension(400,400);
+
+    BorderLayout(String title, String[] buttons){
+        super(title);
+        setSize(dim);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+        setVisible(true);
+    }
 
 }
--- src/com/java21days/Main.java
+++ src/com/java21days/Main.java
@@ -1,4 +1,8 @@
 package com.java21days;
 
 public class Main {
+    public static void main(String[] args) {
+        String[] buttons = { "North", "South", "East", "West" };
+        BorderLayout borderLayout = new BorderLayout("Compass", buttons);
+    }
 }
