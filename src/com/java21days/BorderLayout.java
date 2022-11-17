763a4602cd95a8a08ca45874e1de2a17682b1a7e
Alejandro Salas <asalas.wd@outlook.com>
2022-11-16 20:18:47 -0500

    Add buttons to each position of the compass

--- src/com/java21days/BorderLayout.java
+++ src/com/java21days/BorderLayout.java
@@ -11,9 +11,27 @@ public class BorderLayout extends JFrame {
         setSize(dim);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-
+        JButton[] jButtons = createButtons(buttons);
+        JPanel panel = new JPanel();
+        java.awt.BorderLayout layout = new java.awt.BorderLayout();
+        panel.setLayout(layout);
+        panel.add(jButtons[0], java.awt.BorderLayout.NORTH);
+        panel.add(jButtons[1], java.awt.BorderLayout.SOUTH);
+        panel.add(jButtons[2], java.awt.BorderLayout.EAST);
+        panel.add(jButtons[3], java.awt.BorderLayout.WEST);
+        add(panel);
         setVisible(true);
+    }
+
+    JButton[] createButtons(String[] buttons){
+        JButton[] jButtons = new JButton[buttons.length];
+        int i = 0;
+        for( String text : buttons ){
+            JButton btn = new JButton(text);
+            jButtons[i] = btn;
+            i++;
+        }
+        return jButtons;
     }
 
 }
